d7dec38acd7380c21fc44c1c4bb96e6ed093187c
j0ruge <46424602+j0ruge@users.noreply.github.com>
2021-08-16 22:30:14 -0300

    Added Pressure

---
+++ Classes/Pressure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeTrans.Classes
+{
+    public class Pressure
+    {
+        public double PressaoAbsoluta { get; set; }
+        public double PressaoAtmosferica { get; set; }
+        public double PressaoManometrica { get; set; }
+
+        // O valor sendo vácou devemos usar sinal negativo. 
+        public double CalcularPressaoAbsoluta(double PressaoAtmosferica, double PressaoManometrica)
+        {
+            return PressaoAtmosferica + PressaoManometrica;
+        }
+
+        public string PressaoAbsolutaValorFormatado(double PressaoAtmosferica, double PressaoManometrica)
+        {
+            string result = CalcularPressaoAbsoluta(PressaoAtmosferica, PressaoManometrica).ToString();
+
+            return $"{result} psi";
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FeTrans.Classes;
+using System;
 
 namespace FeTrans
 {
@@ -6,36 +7,11 @@ namespace FeTrans
     {
         static void Main(string[] args)
         {
-            Density density = new Density();
-            density.mass = 22.3;
-            density.volume = 20.3;
+            var pabs = new Pressure();
 
-            double result = density.CalculateDensity();
+            string resultado = pabs.PressaoAbsolutaValorFormatado(14.5, -5.8);
 
-            var mass = new Mass(850.0, 2.0);
-            string massResult = mass.FormatedValue();
-
-            var volume = new Volume(4.0, 5.0, 3.0).CalculateVolume();
-
-            var airDensity = density.densityDictionary["air"];
-
-
-            var ex2Mass = new Mass(airDensity, volume);
-
-            var ex2MassValue = ex2Mass.CalculateMass();
-
-            var ex2AirWeight = new Weight(ex2MassValue);
-
-            var ex2AirWeightValue = ex2AirWeight.CalculateWeight();
-
-            Console.WriteLine(result);
-            Console.WriteLine(density.FormatedDensity());
-            Console.WriteLine(massResult);
-            //Console.WriteLine(volume.FormatedVolume());
-            Console.WriteLine(airDensity);
-            Console.WriteLine(ex2MassValue);
-            Console.WriteLine(ex2AirWeightValue);
-            Console.WriteLine(Math.Pow(5,2));
+            Console.WriteLine(resultado);
         }
     }
 }
